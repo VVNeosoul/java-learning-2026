7ce8c829907c9c709c3519e8bcfe84e4e0b86985
VVNeosoul <y15621460552@gmail.com>
2026-09-16 22:23:16 +0900

    Day 1: Add first Java program

---
+++ day01/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tong";
+        int year = 2026;
+
+        System.out.println("Hello, Java!");
+        System.out.println("My name is " + name);
+        System.out.println("Tokyo Java Job " + year);
+    }
+}
