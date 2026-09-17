cd1d037dfb81be23b891c1e84524acb2401b7514
VVNeosoul <y15621460552@gmail.com>
2026-09-17 22:05:53 +0900

    Day 2: Practice variables and if statements

---
+++ day02/IfPractice.java
@@ -0,0 +1,13 @@
+public class IfPractice {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 80) {
+            System.out.println("Excellent");
+        } else if (score >= 60) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
---
+++ day02/VariablesPractice.java
@@ -0,0 +1,18 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        String name = "Tong";
+        double height = 175.5;
+        boolean isLearningJava = true;
+        char level = 'N';
+
+        int birthYear = 1996;
+        int currentYear = 2026;
+        int age = currentYear - birthYear;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Learning Java: " + isLearningJava);
+        System.out.println("Level: " + level);
+    }
+}
